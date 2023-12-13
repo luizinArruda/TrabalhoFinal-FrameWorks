f2c7a1ac97b068f59a66377b7db12dd88534ab69
luizinArruda <144067062+luizinArruda@users.noreply.github.com>
2023-12-12 23:16:27 -0400

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import styles from '../styles/Components.module.css'
 export default function Footer() {
     return (
         <footer className={styles.footer} >
-            <p>Luiz Henrique, Vinicius da Silva e Ana Leticia Rabelo<br/>
+            <p>Luiz Henrique, Vinicius da Silva<br/>
              CSS - Next - Frameworks2 - IFMS Dourados - Prof. Ricardo Nascimento</p>
         </footer>
     )
