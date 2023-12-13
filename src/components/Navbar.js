d157a4acbe1c686d05e9f243bc1284a4ef75d855
luizinArruda <144067062+luizinArruda@users.noreply.github.com>
2023-12-12 23:22:11 -0400

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import styles from '../styles/Components.module.css'
 export default function Navbar() {
     return (
         <div className={styles.navbar}>
+                <h1>Times</h1>
                <p><a href="\">Home</a></p>
                <p><a href="\times">Times </a></p>
         </div>
